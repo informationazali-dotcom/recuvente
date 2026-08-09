43f19e11ce293deff40002d0ee786bb57b831a07
informationazali-dotcom <informationazali@gmail.com>
2026-08-09 18:44:23 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import SuiviPublicView from "./SuiviPublicView.jsx";
+
+const suiviId = new URLSearchParams(window.location.search).get("suivi");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {suiviId ? <SuiviPublicView commandeId={suiviId} /> : <App />}
   </React.StrictMode>
 );
